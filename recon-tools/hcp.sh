0963ee1a1794abc2e3a7dbf67650f08ae703066a
maedoc <mw@eml.cc>
2016-04-04 10:25:09 +0000

    TVB-2045 hcp-specific script
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7784 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- recon-tools/hcp.sh
+++ recon-tools/hcp.sh
@@ -4,7 +4,15 @@
 # export FSLOUTPUTTYPE=NIFTI_GZ
 # export PATH=/usr/local/fsl/bin/:$PATH
 
+# TODO the act anat prepare fsl is single threaded, so
+#   could be run in parallel over subjects
+#   the rest is IO bound or parallelized within process
+
+# options
 root=/dat4/mw/hcp/single/100307
+nthreads=8
+
+# script starts
 pushd $root
 t1=$root/T1w
 dmr=$t1/Diffusion
@@ -17,22 +25,23 @@ dwi2mask dwi.mif mask.mif
 dwiextract dwi.mif lowb.mif -bzero
 mrconvert lowb.mif lowb.nii.gz
 
-# prep fs results
-mri_convert $t1/aparc+aseg.nii.gz aparc+aseg.nii.gz --out_orientation RAS -rt nearest
-fslreorient2std aparc+aseg.nii.gz aparc+aseg-ro.nii.gz
-mri_convert $t1/T1w_acpc_dc_restore.nii.gz T1.nii.gz --out_orientation RAS
+# vol dmr analysis
+dwi2response -nthreads $nthreads dwi.mif response.txt -mask mask.mif -sf sf-mask.mif
+dwi2fod -nthreads $nthreads dwi.mif response.txt csd.mif -mask mask.mif
+act_anat_prepare_fsl $t1/T1w_acpc_dc_restore.nii.gz act.mif
+5tt2gmwmi act.mif gmwmi.mif
 
-# register
-flirt -in lowb.nii.gz -ref T1.nii.gz -omat d2t.mat -out lowb-in-t1.nii.gz -cost mutualinfo
-convert_xfm -omat t2d.mat -inverse d2t.mat
-flirt -applyxfm -in aparc+aseg.nii.gz -ref lowb.nii.gz -out aparc+aseg-in-d.nii.gz -init t2d.mat -interp nearestneighbour
-flirt -applyxfm -in T1.nii.gz -ref lowb.nii.gz -out t1-in-d.nii.gz -init t2d.mat
+# tracking
+tckgen -nthreads $nthreads -num 10000000 -seed_gmwmi gmwmi.mif -act act.mif -unidirectional -maxlength 250 -step 0.5 csd.mif brain.tck
+tcksift brain.tck csd.mif brain-sift.tck -act act.mif -term_number 2000000
 
-# register
-flirt -in T1.nii.gz -ref lowb.nii.gz -omat t2d.mat -out T1-d.nii.gz -cost mutualinfo
-flirt -applyxfm -in aparc+aseg.nii.gz -ref lowb.nii.gz -out aparc+aseg-d.nii.gz -init t2d.mat -interp nearestneighbour
-
-# freeview -v T1-d.nii.gz lowb.nii.gz:colormap=heat aparc+aseg-d.nii.gz:colormap=jet
+# connectome
+conf=$(dirname $(dirname $(which mrconvert)))/src/connectome/config/fs_default.txt
+labelconfig $t1/aparc+aseg.nii.gz $conf aparc+aseg.mif -lut_freesurfer ${FREESURFER_HOME}/FreeSurferColorLUT.txt
+for metric in count meanlength
+do
+	tck2connectome brain-sift.tck aparc+aseg.mif $metric.csv -assignment_radial_search 2 -zero_diagonal -metric $metric
+done
 
 popd # TVB
 popd # $root
