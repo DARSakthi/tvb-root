b43a62cabd97d17fbcf1e87b7bec71a9ab27a3ca
liadomide <lia_domide@yahoo.com>
2016-11-08 12:59:07 +0000

    Move BNM tools on Github completely https://github.com/ins-amu/bnm-recon-tools
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8154 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- recon-tools/compute_gdist.py
+++
@@ -1,9 +0,0 @@
-import nibabel, gdist, scipy.io, os
-SUBJECTS_DIR, SUBJECT = [os.environ[key] for key in ['SUBJECTS_DIR', 'SUBJECT']]
-for h in 'rl':
-  surf_path = '%s/%s/surf/%sh.pial.low' % (SUBJECTS_DIR, SUBJECT, h)
-  v, f = nibabel.freesurfer.read_geometry(surf_path)
-  mat_path = '%s/%s/surf/%sh.pial.low.gdist.mat' % (SUBJECTS_DIR, SUBJECT, h)
-  mat = gdist.local_gdist_matrix(v, f.astype('<i4'), max_distance=40.0)
-  scipy.io.savemat(mat_path, {'gdist': mat})
-
